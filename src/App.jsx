8c9272b9c3177d602646c3bcb8c0770f072e7a7f
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-06-29 17:12:02 +0600

    useNavigate needs to be appied within the BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
 import Signup from "./Components/Signup";
 import Appbar from "./Components/Appbar";
 import Signin from "./Components/Signin";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
-
-// import BrowserRouter as Router means that we are importing 
+// import BrowserRouter as Router means that we are importing
 // BrowserRouter as Router from the react-router-dom library.
 
 function App() {
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-      backgroundColor: "#eeeeee ",
-    
-    }}>
-      <Appbar />
-      <Router>
+    <BrowserRouter>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "#eeeeee ",
+        }}
+      >
+        <Appbar />
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Signin />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
