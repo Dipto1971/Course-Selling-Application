94b67a98b5dc36e4497157e3704fb8a0a12b9a6e
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-06-29 16:03:33 +0600

    .

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Button, Typography } from "@mui/material";
+import React from "react";
 
 const Appbar = () => {
   return (
-    <div>
-        This is Appbar
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px",
+        backgroundColor: "#000",
+        color: "#fff",
+      }}
+    >
+      <div>
+        <Typography> Coursera </Typography>
+      </div>
+      <div style={{
+        marginRight: "100px",
+      }}>
+        <Button color="inherit">Login</Button>
+        <Button color="inherit">Signup</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appbar
+export default Appbar;
