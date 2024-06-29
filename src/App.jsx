11eb2c3c3fbe2b44fc6532475fa2a21296fb31f6
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-06-29 15:50:43 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import Signup from './Componentsss/Signup'
+import { useState } from "react";
+import Signup from "./Componentsss/Signup";
+import { AppBar } from "@mui/material";
+import Appbar from "./Componentsss/Appbar";
 
 function App() {
   return (
-    <div style={{
-      backgroundColor: "#eeeeee",
-    }}>
-    <Signup /> 
+    <div
+      style={{
+        backgroundColor: "#eeeeee",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Appbar />
+      <Signup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
