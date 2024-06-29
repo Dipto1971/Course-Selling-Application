2855f2c0f3437691b4066d135ad9d422a119fdfa
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-06-29 15:48:09 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Signup from './Componentsss/Signup'
 
 function App() {
   return (
-    <>
+    <div style={{
+      backgroundColor: "#eeeeee",
+    }}>
     <Signup /> 
-    </>
+    </div>
   )
 }
 
--- src/Componentsss/Signup.jsx
+++ src/Componentsss/Signup.jsx
@@ -1,14 +1,44 @@
-import { Button, TextField } from '@mui/material'
-import React from 'react'
+import { Button, Card, TextField, Typography } from "@mui/material";
+import React from "react";
 
 const Signup = () => {
   return (
-    <div>
-        <TextField id="outlined-basic" label="Username" variant="outlined" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
-        <Button variant="outlined" >Signup</Button>
+    <div
+      style={{
+        width: "400px",
+        margin: "auto",
+        padding: "20px",
+        marginTop: "100px",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant={"h5"} align="center">Welcome to Coursera!</Typography>
+      <Card
+        varient={"outlined"}
+        style={{
+          width: "400px",
+          margin: "auto",
+          padding: "20px",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          fullWidth
+        />
+        <br /> <br />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          fullWidth
+        />
+        <br /> <br />
+        <Button variant="contained">Signup</Button>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
