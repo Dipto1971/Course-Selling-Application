cd67a11040fe22d463a3fbe03e489a2dd3f5049b
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-07-04 11:38:40 +0600

    Added useEffect hook in appbar

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -1,10 +1,30 @@
 import { Button, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Appbar = () => {
-
   const navigateTo = useNavigate();
+  const [userEmail, setUserEmail] = React.useState("");
+
+  useEffect(() => {
+    function callback2(data) {
+      if (data.usename) {
+        setUserEmail(data.username);
+      }
+      console.log(data);
+    }
+
+    function callback1(response) {
+      response.json().then(callback2);
+    }
+
+    fetch("http://localhost:3000/admin/me", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    }).then(callback1);
+  }, []);
 
   return (
     <div
@@ -27,7 +47,7 @@ const Appbar = () => {
         <Button
           color="inherit"
           onClick={() => {
-            navigateTo("/login")
+            navigateTo("/login");
           }}
         >
           Login
@@ -35,7 +55,7 @@ const Appbar = () => {
         <Button
           color="inherit"
           onClick={() => {
-            navigateTo("/signup")
+            navigateTo("/signup");
           }}
         >
           Signup
