daa4cff37050f3a652fb3163d4254e70fbadf3c3
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-07-04 11:31:29 +0600

    showing courses in courses.jsx page

---
+++ src/Courses.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useEffect } from "react";
+const Courses = () => {
+  const [courses, setCourses] = React.useState([]);
+
+  const callback1 = (response) => {
+    response.json().then((data) => {
+        console.log(data);
+        setCourses(data.courses);
+    });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3000/admin/courses", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    }).then(callback1);
+  }, []);
+
+  return <div>
+    {courses.map((course) => {
+      return <div>
+        <h1>{course.title}</h1>
+        <p>{course.description}</p>
+      </div>;
+    })}
+  </div>;
+};
+
+export default Courses;
