df9efde7b8e049b98a22bdd3e9fc1865755cec05
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-07-04 12:36:24 +0600

    admin.js refignments for sigin options

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -18,7 +18,7 @@ router.get("/me", authenticateJwt, async (req, res) => {
     })
 });
 
-router.GET("/admin/me", authenticateJwt, (req, res) => {
+router.get("/admin/me", authenticateJwt, (req, res) => {
   res.json ({
     username: req.user.username
   })
@@ -43,7 +43,7 @@ router.post('/signup', (req, res) => {
   });
   
   router.post('/login', async (req, res) => {
-    const { username, password } = req.headers;
+    const { username, password } = req.body;
     const admin = await Admin.findOne({ username, password });
     if (admin) {
       const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
