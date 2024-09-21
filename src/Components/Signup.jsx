2ddab6ffae77b96ed45b23dfe46935743addf890
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-21 09:30:00 +0600

    modified signup component

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,86 +1,64 @@
-import { Button, Card, TextField, Typography } from "@mui/material";
-import React from "react";
-import { useState } from "react";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import {Card, Typography} from "@mui/material";
+import {useState} from "react";
+import axios from "axios";
 
-const Signup = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [successfullMsg, setSuccessfullMsg] = useState("");
+function Signup() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
-  return (
-    <div
-      style={{
-        width: "400px",
-        margin: "auto",
-        padding: "20px",
-        marginTop: "100px",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant={"h5"} align="center">
-        Welcome to Coursera!
-      </Typography>
-      <Card
-        varient={"outlined"}
-        style={{
-          width: "400px",
-          margin: "auto",
-          padding: "20px",
-        }}
-      >
-        <TextField
-          onChange={(e) => setUsername(e.target.value)}
-          id={"usename"}
-          label="Username"
-          variant="outlined"
-          fullWidth
-        />
-        <br /> <br />
-        <TextField
-          onChange={(e) => {
-            setPassword(e.target.value);
-            console.log(e);
-          }}
-          id={"password"}
-          label="Password"
-          variant="outlined"
-          fullWidth
-        />
-        <br /> <br />
-        <Button
-          variant="contained"
-          onClick={() => {
-            // Connection to backend
-            function callbackToken(data) {
-              localStorage.setItem("token", data.token);
-            }
+    return <div>
+            <div style={{
+                paddingTop: 150,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <Typography variant={"h6"}>
+                Welcome to Coursera. Sign up below
+                </Typography>
+            </div>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+                <TextField
+                    onChange={(evant11) => {
+                        let elemt = evant11.target;
+                        setUsername(elemt.value);
+                    }}
+                    fullWidth={true}
+                    label="Email"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Password"
+                    variant="outlined"
+                    type={"password"}
+                />
+                <br/><br/>
 
-            fetch("http://localhost:3000/admin/signup", {
-              method: "POST",
-              headers: {
-                "Content-type": "application/json",
-              },
-              body: JSON.stringify({ username: username, password: password }),
-            })
+                <Button
+                    size={"large"}
+                    variant="contained"
+                    onClick={async() => {
+                        const response = await axios.post("http://localhost:3000/admin/signup", {
+                            username: username,
+                            password: password
+                        })
+                        let data = response.data;
+                        localStorage.setItem("token", data.token);
+                        window.location = "/"
+                    }}
 
-            .then((res) => {
-              res.status === 200
-                ? setSuccessfullMsg("Admin created successfully")
-                : setSuccessfullMsg(res.message);
-
-                res.json().then(callbackToken)
-            });
-          }}
-        >
-          Sign up
-        </Button>
-
-        <Typography variant={"h6"} align="center">
-          {successfullMsg}
-        </Typography>
-      </Card>
+                > Signup</Button>
+            </Card>
+        </div>
     </div>
-  );
-};
+}
 
 export default Signup;
