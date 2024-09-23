c49d3a8ebb9be0ac68a5a4aff7fe1ec9845d2811
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-23 08:54:59 +0600

    .

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,59 +1,86 @@
 import { Button, Card, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import React from "react";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
 
-  useEffect(() => {
-    const fetchdata = async () => {
-      try {
-        const res = await axios.get("http://localhost:3000/admin/courses", {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        });
-        const data = res.data;
-        setCourses(data.courses);
+  // Checking if the user is logged in
+  if (!localStorage.getItem("token") || localStorage.getItem("token") == 'null') {
+    return (
+      <div>
+        <h1>You are not logged in</h1>
+      </div>
+    );
+  }
+    useEffect(() => {
+      const fetchdata = async () => {
+        try {
+          const res = await axios.get("http://localhost:3000/admin/courses", {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          });
+          const data = res.data;
+          setCourses(data.courses);
 
-        console.log(data);
-      } catch (e) {
-        console.log(e);
+          console.log(data);
+        } catch (e) {
+          console.log(e);
+        }
       }
-    };
-    fetchdata();
-  }, []);
+      fetchdata();
+    }, []);
 
-  return <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
-        {courses.map((course, index) => {
-            return <Course course={course} key={index} />}
-        )}
+  return (
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
+      {courses.map((course, index) => {
+        return <Course course={course} key={index} />;
+      })}
     </div>
-}
+  );
+};
 
-export function Course({course}) {
-    const navigate = useNavigate();
+export function Course({ course }) {
+  const navigate = useNavigate();
 
-    return <Card style={{
+  return (
+    <Card
+      style={{
         margin: 10,
         width: 300,
         minHeight: 200,
-        padding: 20
-    }}>
-        <Typography textAlign={"center"} variant="h5">{course.title}</Typography>
-        <Typography textAlign={"center"} variant="subtitle1">{course.description}</Typography>
-        <img src={course.imageLink} style={{width: 300}} alt={course.title}></img>
-        <div style={{display: "flex", justifyContent: "center", marginTop: 20}}>
-            <Button variant="contained" size="large" onClick={() => {
-                navigate("/course/" + course._id);
-            }}>Edit</Button>
-        </div>
+        padding: 20,
+      }}
+    >
+      <Typography textAlign={"center"} variant="h5">
+        {course.title}
+      </Typography>
+      <Typography textAlign={"center"} variant="subtitle1">
+        {course.description}
+      </Typography>
+      <img
+        src={course.imageLink}
+        style={{ width: 300 }}
+        alt={course.title}
+      ></img>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => {
+            navigate("/course/" + course._id);
+          }}
+        >
+          Edit
+        </Button>
+      </div>
     </Card>
-
+  );
 }
-
-
 
 export default Courses;
