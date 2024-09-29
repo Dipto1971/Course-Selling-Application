c00f22ba4b3c4e178df8ef130b742abd37bad879
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-29 12:27:34 +0600

    Built selectors of the atoms

--- client/src/store/selectors/course.js
+++ client/src/store/selectors/course.js
@@ -1,12 +1,42 @@
 import {selector} from 'recoil';
 import {courseState} from '../atoms/course';
 
-export const isCourseLoading = selector ({});
+export const isCourseLoading = selector ({
+    key: 'isCourseLoadingState',
+    get: ({get}) => {
+        const course = get(courseState);
+        return course.isLoading;
+    }
+});
 
-export const courseDetails = selector({});
+export const courseDetails = selector({
+    key: 'courseDetailsState',
+    get: ({get}) => {
+        const course = get(courseState);
+        return course.course;
+    }
+});
 
-export const courseTitle = selector ({});
+export const courseTitle = selector ({
+    key: 'courseTitleState',
+    get: ({get}) => {
+        const course = get(courseState);
+        return course.course.title;
+    }
+});
 
-export const coursePrice = selector ({});
+export const coursePrice = selector ({
+    key: 'coursePriceState',
+    get: ({get}) => {
+        const course = get(courseState);
+        return course.course.price;
+    }
+});
 
-export const courseImage = selector ({});
+export const courseImage = selector ({
+    key: 'courseImageState',
+    get: ({get}) => {
+        const course = get(courseState);
+        return course.course.image;
+    }
+});
---
+++ client/src/store/selectors/userEmail.js
@@ -0,0 +1,10 @@
+import { userState} from '../atoms/user';
+import { selector } from 'recoil';
+
+export const userEmail = selector({
+    key: 'userEmailState',
+    get: ({get}) => {
+        const user = get(userState);
+        return user.userEmail;
+    }
+});
