2800bb7b9e8c7a2ad663306adaa8321525a7b544
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-29 10:22:08 +0600

    Added selectors

---
+++ client/src/store/selectors/course.js
@@ -0,0 +1,12 @@
+import {selector} from 'recoil';
+import {courseState} from '../atoms/course';
+
+export const isCourseLoading = selector ({});
+
+export const courseDetails = selector({});
+
+export const courseTitle = selector ({});
+
+export const coursePrice = selector ({});
+
+export const courseImage = selector ({});
