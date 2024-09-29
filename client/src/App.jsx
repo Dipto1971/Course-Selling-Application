56f40abf9db26fa0f25ab9ca46356f4fb94d322f
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-29 10:18:43 +0600

    File Structuring

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import Signup from "./Components/Signup";
+import Appbar from "./Components/Appbar";
+import Signin from "./Components/Signin";
+import AddCourse from "./Components/AddCourse";
+import Course from "./Components/Course";
+import Courses from "./Courses";
+
+import {
+  BrowserRouter,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "#eeeeee ",
+        }}
+      >
+        <Appbar />
+        <Routes>
+          <Route path="/addcourse" element={<AddCourse />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/course/:courseId" element={<Course />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
