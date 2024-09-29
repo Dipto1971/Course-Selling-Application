1738f0ecf85130897b0852c3604ffa409b7f2d5d
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-09-29 13:15:01 +0600

    Refactor component imports and add user initialization
    - Refactored component imports in App.jsx to use correct file paths and naming conventions.
    - Added a new component, InitUser, to initialize the user state using Recoil and make an API call to retrieve the user's information.
    - The InitUser component is called within the App component and sets the user state based on the response from the API call.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,76 @@
-import Signup from "./Components/Signup";
-import Appbar from "./Components/Appbar";
-import Signin from "./Components/Signin";
-import AddCourse from "./Components/AddCourse";
-import Course from "./Components/Course";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signin from "./components/Signin.jsx";
+import Signup from "./Components/Signup.jsx";
+import AppBar from "./Components/Appbar.jsx";
+import AddCourse from "./Components/AddCourse.jsx";
 import Courses from "./Courses";
-
+import Course from "./Components/Course";
+import {Landing} from "./Components/Landing.jsx";
+import { userState } from "./store/atoms/user.js";
 import {
-  BrowserRouter,
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
-
+    RecoilRoot,
+    useSetRecoilState
+} from 'recoil';
+import axios from "axios";
+import {BASE_URL} from "./config.js";
+import {useEffect} from "react";
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <RecoilRoot>
       <div
         style={{
           width: "100vw",
-          height: "100vh",
-          backgroundColor: "#eeeeee ",
+          backgroundColor: "#eeeeee",
         }}
       >
-        <Appbar />
-        <Routes>
-          <Route path="/addcourse" element={<AddCourse />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/course/:courseId" element={<Course />} />
-        </Routes>
+        <Router>
+          <AppBar />
+          <InitUser />
+          <Routes>
+            
+            <Route path={"/addcourse"} element={<AddCourse />} />
+            <Route path={"/course/:courseId"} element={<Course />} />
+            <Route path={"/courses"} element={<Courses />} />
+            <Route path={"/signin"} element={<Signin />} />
+            <Route path={"/signup"} element={<Signup />} />
+            <Route path={"/"} element={<Landing />} />
+          </Routes>
+        </Router>
       </div>
-    </BrowserRouter>
+    </RecoilRoot>
   );
+}
+
+function InitUser() {
+  const setUser = useSetRecoilState(userState);
+  const init = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/admin/me`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (response.data.username) {
+        setUser({
+          isLoading: false,
+          userEmail: response.data.username,
+        });
+      }
+    } catch (e) {
+      setUser({
+        isLoading: false,
+        userEmail: null,
+      });
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
